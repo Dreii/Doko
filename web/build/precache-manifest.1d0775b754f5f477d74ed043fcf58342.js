1baad793bc072f49d8cab20c8d2ba300354a0b97
Sean Verhaagen <seanverhaagen@gmail.com>
2019-06-22 12:43:57 -0500

    - Added build folder to git

---
+++ web/build/precache-manifest.1d0775b754f5f477d74ed043fcf58342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca8e0738e2391018e1cc95d822f4211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3888846f9c4e6b4fe573",
+    "url": "/static/css/main.df103992.chunk.css"
+  },
+  {
+    "revision": "b66606ffb151d00211e1",
+    "url": "/static/js/2.3d93a094.chunk.js"
+  },
+  {
+    "revision": "3888846f9c4e6b4fe573",
+    "url": "/static/js/main.3bb5ac93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
