8dfeb699a27da1bf8e2390ce178a00834fb34549
Sean Verhaagen <seanverhaagen@gmail.com>
2019-08-10 23:49:05 -0500

    logging process

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const app = require('./functions/setup')
 const https = require('https')
 const fs = require('fs')
 
+console.log(process.env)
+
 https.createServer({
   key: fs.readFileSync(process.env.NODE_SERVER_KEY),
   cert: fs.readFileSync(process.env.NODE_SERVER_CERT)
